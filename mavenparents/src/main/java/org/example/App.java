349da610d5f8275ea1215107129535410b1d6e50
Layne <Layne@atguigu.com>
2022-11-24 13:41:03 +0800

    forth2 commit

--- mavenparents/src/main/java/org/example/App.java
+++ mavenparents/src/main/java/org/example/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("good");
         System.out.println("nice");
+        System.out.println("5");
     }
 }
